b319b141a73d75435fb4c30092cdc4cced13b067
Luna Gava <lunagava@me.com>
2021-07-29 15:02:01 +0000

    Modified interpolation according to sampling period.

--- python/ground_truth.py
+++ python/ground_truth.py
@@ -1,15 +1,16 @@
-import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
 x = []
 y = []
 timestamp = []
-set_points = []
 couple = []
-n_points = 100
+# n_points = 10
 bb_width = 10
 bb_height = 10
+getPeriod = 10  # in ms
+line_index = 0
+first_time = True
 
 out_file = open("points.csv", "w")
 
@@ -26,6 +27,9 @@ def compute_deltaTime(start, end, num_points):
     delta_t = (end - start) / num_points
     return delta_t
 
+def compute_n_points(start, end, period):
+    period = (end-start)/period
+    return period
 
 filePath = sys.argv[1]
 
@@ -41,21 +45,29 @@ for i in range(len(CoM_timestamp)):
 
     count += 1
 
-    if count == 2:
-
-        count = 0
-
-        data = [xCoM[i - 1], yCoM[i - 1]]
+    if not first_time:
+        couple.clear()
+        data = [xCoM[i-1], yCoM[i-1]]
         couple.append(data)
         data = [xCoM[i], yCoM[i]]
         couple.append(data)
 
-        print(data)
+    if first_time:
+        if count == 2:
+            data = [xCoM[i - 1], yCoM[i - 1]]
+            couple.append(data)
+            data = [xCoM[i], yCoM[i]]
+            couple.append(data)
+            first_time = False
+
+    if not first_time:
 
         x_new = xCoM[i - 1]
 
         currentT = CoM_timestamp[i - 1]
-        deltaT = compute_deltaTime(CoM_timestamp[i - 1], CoM_timestamp[i], n_points)
+        # deltaT = compute_deltaTime(CoM_timestamp[i - 1], CoM_timestamp[i], n_points)
+
+        n_points = compute_n_points(CoM_timestamp[i - 1], CoM_timestamp[i], getPeriod)
 
         x_factor = abs(xCoM[i - 1] - xCoM[i]) / n_points
         if (couple[1][0] - couple[0][0]) == 0:
@@ -80,30 +92,26 @@ for i in range(len(CoM_timestamp)):
             bottom_bb = int(round(y_new - bb_height / 2))
             top_bb = int(round(y_new + bb_height / 2))
 
-            out_file.write(str(left_bb) + ',' + str(bottom_bb) + ',' + str(right_bb) + ',' + str(top_bb) + ',' + str(
-                int(round(currentT))) + ',' + str(1) + '\n')
+            # out_file.write(str(left_bb) + ',' + str(bottom_bb) + ',' + str(right_bb) + ',' + str(top_bb) + ',' + str(
+            #     int(round(currentT))) + ',' + str(1) + '\n')
+
+            out_file.write(str(line_index) + ' ' + str(0.001*int(round(currentT))) + ' ' + str(round(x_new)) + ' ' + str(round(y_new)) + ' ' + str(0.001*int(round(currentT))) + '\n')
+            print(str(round(x_new)) + " " + str(round(y_new)) + " " + str(0.001*int(round(currentT))))
 
             if xCoM[i - 1] > xCoM[i]:
                 x_new = x_new - x_factor
             else:
                 x_new = x_new + x_factor
 
-            currentT = currentT + deltaT
+            currentT = currentT + getPeriod
 
-            set_points.append(point)
             counter_points += 1
 
-        # xp = [x[0] for x in set_points]
-        # yp = [x[1] for x in set_points]
-        # plt.plot(xp, yp, 'ro')
-        # plt.axis([0, 304, 0, 240])
-        # plt.show()
+            line_index += 1
 
-        set_points.clear()
         x.clear()
         y.clear()
         timestamp.clear()
         data.clear()
-        couple.clear()
 
 out_file.close()
