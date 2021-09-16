f39cb61cabab18f351b68a3f263c102d93f26776
Luna Gava <luna.gava@iit.it>
2021-09-16 14:25:37 +0000

    code to visualize heat map based on the number of events received by each pixel.

--- python/read_ego_motion_events.py
+++ python/read_ego_motion_events.py
@@ -1,16 +1,43 @@
 import numpy as np
 from PIL import Image
 from IPython.display import display
+import matplotlib.pyplot as plt
 
-file = np.genfromtxt("/root/.local/share/yarp/contexts/event-driven/egoMotion.tsv", delimiter="\t", names=["x", "y"])
+A = []
+with open('/code/luna/study-air-hockey/heat_map.txt', 'r') as f:
+    for line in f:
+        A.append(list(map(int, line.split())))
+
+max_value, max_index = max((x, (i, j))
+                           for i, row in enumerate(A)
+                           for j, x in enumerate(row))
+
+print(max_value)
+
+fig = plt.figure()
+im = plt.imshow(A, cmap="gist_stern")
+
+# plt.gca().invert_xaxis()
+plt.gca().invert_yaxis()
+plt.xticks(rotation=90)
+plt.yticks(rotation=90)
+plt.ylabel("x")
+plt.xlabel("y")
+cbar = fig.colorbar(im, orientation="vertical")
+plt.show()
+
+file = np.genfromtxt("/root/.local/share/yarp/contexts/event-driven/egoMotion_50.tsv", delimiter="\t", names=["x", "y"])
 
 x = file["x"]
 y = file["y"]
 
-MyImg = Image.new( 'RGB', (304, 240), "black")
-pixels = MyImg.load() # creates the pixel map
+MyImg = Image.new('RGB', (304, 240), "black")
+pixels = MyImg.load()  # creates the pixel map
 
 for i in range(len(x)):
     pixels[int(x[i]), int(y[i])] = (255, 255, 255)
 
 MyImg.show()
+
+MyImg.save("events_suppressed.png")
+
